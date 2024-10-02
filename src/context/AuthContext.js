1b0ac6d8683c20ab901ca8d0c350894a00c0b593
yangle0306 <yangle0306@gmail.com>
2024-10-02 11:02:10 +0900

    Feat: 간단하게 로그인 성공 구현
    로컬 스토리지에 저장

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,18 +1,26 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 // Context 생성
 export const AuthContext = createContext();
 
 // Context Provider 컴포넌트
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 관리
+  // 로컬 스토리지에서 로그인 상태를 가져옴, 없으면 false로 초기화
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    () => JSON.parse(localStorage.getItem("isLoggedIn")) || false
+  );
+
+  // 로그인 상태가 변경될 때 로컬 스토리지에 저장
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   const login = () => {
-    setIsLoggedIn(true); // 로그인 상태 true
+    setIsLoggedIn(true); // 로그인 상태 true로 설정
   };
 
   const logout = () => {
-    setIsLoggedIn(false); // 로그아웃 처리 (로그인 상태 false)
+    setIsLoggedIn(false); // 로그인 상태 false로 설정
   };
 
   return (
