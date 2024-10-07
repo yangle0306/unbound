221d5d3e8eaf1a64a3db4a2fca4197e22225ee97
yangle0306 <yangle0306@gmail.com>
2024-10-07 13:26:54 +0900

    Feat: 지원완료 구현

---
+++ src/components/ApplyConfirmation.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import ApplyConfirmationSVG from "../assets/applyconfirmation.svg"; // SVG 파일을 불러옵니다.
+
+// 500x272 크기의 둥근 직사각형 컨테이너
+const ConfirmationContainer = styled.div`
+  width: 500px;
+  height: 272px;
+  background-color: #ffffff;
+  border-radius: 20px; /* 둥근 모서리 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 20px;
+`;
+
+// 텍스트와 이미지를 감싸는 컨테이너
+const TextAndImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px; /* 텍스트와 이미지 사이의 간격 */
+  margin-bottom: 20px;
+`;
+
+// 104x104 크기의 이미지 스타일
+const ConfirmationImage = styled.img`
+  width: 104px;
+  height: 104px;
+`;
+
+// "지원되었습니다" 텍스트 스타일
+const ConfirmationText = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  color: #1e388b;
+  text-align: left;
+`;
+
+// 추가 메시지 텍스트 스타일
+const AdditionalMessageText = styled.p`
+  font-size: 15px;
+  font-weight: 500;
+  color: #838383;
+  margin-top: 10px;
+`;
+
+// 확인 버튼 스타일
+const ConfirmButton = styled.button`
+  margin-top: 20px;
+  width: 460px;
+  height: 45px;
+  background-color: #1e388b;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3f5ba9;
+  }
+`;
+
+const ApplyConfirmation = ({ onConfirm }) => {
+  return (
+    <ConfirmationContainer>
+      {/* 텍스트와 이미지를 같은 줄에 배치 */}
+      <TextAndImageContainer>
+        <ConfirmationImage src={ApplyConfirmationSVG} alt="확인 이미지" />
+        <div>
+          <ConfirmationText>지원되었습니다</ConfirmationText>
+          <AdditionalMessageText>
+            담당자 배정 후 연락드리겠습니다.
+          </AdditionalMessageText>
+        </div>
+      </TextAndImageContainer>
+
+      {/* 확인 버튼 */}
+      <ConfirmButton onClick={onConfirm}>확인</ConfirmButton>
+    </ConfirmationContainer>
+  );
+};
+
+export default ApplyConfirmation;
--- src/components/ApplyPrompt.js
+++ src/components/ApplyPrompt.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ApplyPromptSVG from "../assets/applyprompt.svg";
+import ApplyConfirmation from "./ApplyConfirmation";
 
 // 350x586 크기의 둥근 직사각형 컨테이너
 const RoundedContainer = styled.div`
@@ -56,14 +57,14 @@ const ContentGroup = styled.div`
 // 모집 제목 텍스트 스타일
 const JobTitleText = styled.h2`
   font-size: 24px;
-  font-weight: medium;
+  font-weight: 500;
   color: #313131;
 `;
 
 // 메시지 텍스트 스타일
 const MessageText = styled.h2`
   font-size: 14px;
-  font-weight: medium;
+  font-weight: 500;
   color: #313131;
 `;
 
@@ -77,7 +78,7 @@ const DescriptionText = styled.p`
 // 링크 텍스트 스타일
 const LinkText = styled.a`
   font-size: 12px;
-  font-weight: medium;
+  font-weight: 500;
   color: #1e388b;
   cursor: pointer;
   text-decoration: none;
@@ -113,38 +114,50 @@ const Button = styled.button`
 
 // ApplyPrompt 컴포넌트
 const ApplyPrompt = ({ onApply, onCancel }) => {
+  const [isSubmitted, setIsSubmitted] = useState(false); // 모달 상태 관리
+
+  const handleApply = () => {
+    setIsSubmitted(true); // "지원되었습니다" 모달로 전환
+  };
+
   return (
-    <RoundedContainer>
-      <ApplyPromptContainer>
-        {/* SVG 파일을 이미지로 삽입 */}
-        <Image src={ApplyPromptSVG} alt="지원 이미지" />
+    <>
+      {isSubmitted ? (
+        <ApplyConfirmation onConfirm={onCancel} /> // "지원되었습니다" 모달 표시
+      ) : (
+        <RoundedContainer>
+          <ApplyPromptContainer>
+            {/* SVG 파일을 이미지로 삽입 */}
+            <Image src={ApplyPromptSVG} alt="지원 이미지" />
 
-        {/* Content 그룹으로 텍스트 묶기 */}
-        <ContentContainer>
-          {/* 모집 제목과 메시지 묶기 */}
-          <ContentGroup>
-            <JobTitleText>모집공고제목</JobTitleText>
-            <MessageText>지원하시겠습니까?</MessageText>
-          </ContentGroup>
+            {/* Content 그룹으로 텍스트 묶기 */}
+            <ContentContainer>
+              {/* 모집 제목과 메시지 묶기 */}
+              <ContentGroup>
+                <JobTitleText>모집공고제목</JobTitleText>
+                <MessageText>지원하시겠습니까?</MessageText>
+              </ContentGroup>
 
-          {/* 설명 텍스트와 이력서 수정 링크 묶기 */}
-          <ContentGroup>
-            <DescriptionText>
-              이력서가 전달됩니다. 마지막으로 이력서를 확인하세요.
-            </DescriptionText>
-            <LinkText href="/resume/edit">이력서 수정</LinkText>
-          </ContentGroup>
-        </ContentContainer>
+              {/* 설명 텍스트와 이력서 수정 링크 묶기 */}
+              <ContentGroup>
+                <DescriptionText>
+                  이력서가 전달됩니다. 마지막으로 이력서를 확인하세요.
+                </DescriptionText>
+                <LinkText href="/resume/edit">이력서 수정</LinkText>
+              </ContentGroup>
+            </ContentContainer>
 
-        {/* 버튼 컨테이너 */}
-        <ButtonContainer>
-          <Button $primary onClick={onCancel}>
-            아니오
-          </Button>
-          <Button onClick={onApply}>지원</Button>
-        </ButtonContainer>
-      </ApplyPromptContainer>
-    </RoundedContainer>
+            {/* 버튼 컨테이너 */}
+            <ButtonContainer>
+              <Button $primary onClick={onCancel}>
+                아니오
+              </Button>
+              <Button onClick={handleApply}>지원</Button>
+            </ButtonContainer>
+          </ApplyPromptContainer>
+        </RoundedContainer>
+      )}
+    </>
   );
 };
 
